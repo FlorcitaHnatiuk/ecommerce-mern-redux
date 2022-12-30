3e7199dea110f84b301a575b2b4e8ea5787461e3
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2022-12-30 14:54:18 -0300

    order screen + model + route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
+import orderRouter from './routers/orderRouter.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/moov', {
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
 });
