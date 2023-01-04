8014cecc51436008058a5b285a4b9d677fd19e11
Florcita Hnatiuk <flopi.hnatiuk@gmail.com>
2023-01-04 15:26:07 -0300

    Admin actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,8 @@ import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SigninScreen from './screens/SigninScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
+import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -34,7 +36,7 @@ function App() {
         <header className="row">
           <div>
             <Link className="brand" to="/">
-              MOOV
+              amazona
             </Link>
           </div>
           <div>
@@ -116,9 +118,14 @@ function App() {
             path="/orderlist"
             component={OrderListScreen}
           ></AdminRoute>
+          <AdminRoute path="/userlist" component={UserListScreen}></AdminRoute>
+          <AdminRoute
+            path="/user/:id/edit"
+            component={UserEditScreen}
+          ></AdminRoute>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
-        <footer className="row center">Coded with ♥ by Flor Hnatiuk</footer>
+        <footer className="row center">All right reserved</footer>
       </div>
     </BrowserRouter>
   );
